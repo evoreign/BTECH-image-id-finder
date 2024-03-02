b9cb1e847e201d3aff482507e86927ae110bc1c7
evoreign <edbert_khovey@yahoo.com>
2024-03-02 20:51:10 +0700

    File uploader FE

--- FE/btech/app/doc-upload/page.tsx
+++ FE/btech/app/doc-upload/page.tsx
@@ -1,19 +1,35 @@
 "use client"
+import { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import Header from "@/components/navbar-header";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 export default function Doc() {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    // onDrop is the function that will be called when files are dropped onto the dropzone
+    onDrop: (acceptedFiles) => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+    }
+  });
+
+  const filesList = files.map(file => (
+    <li key={file.name}>
+      {file.name} - {file.type}
+    </li>
+  ));
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <Header/>
-      <div >
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
-          DOC uploader here
-        </h1>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Input id="file" type="file" />
-        </div>
+      <div {...getRootProps()} className={`border-dashed border-2 ${isDragActive ? 'border-green-600 bg-green-100' : 'border-gray-600'} p-10 w-full max-w-xl items-center gap-1.5 rounded-2xl h-96 flex flex-col justify-center items-center`}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+        <ul>{filesList}</ul>
       </div>
     </main>
   );
--- FE/btech/components/navbar-header.tsx
+++ FE/btech/components/navbar-header.tsx
@@ -34,10 +34,10 @@ const Header: React.FC = () => {
   }
 
   return (
-    <header className={`w-full p-2 sm:p-4 fixed top-0 left-0 flex justify-between items-center px-2 sm:px-4 py-1 sm:py-2 bg-white text-${theme === 'dark' ? 'white' : 'black'} z-50`}>
+    <header className={`w-full p-2 sm:p-4 fixed top-0 left-0 flex justify-between items-center px-2 sm:px-4 py-1 sm:py-2 bg-transparent text-${theme === 'dark' ? 'white' : 'black'} z-50`}>
       <div className="flex items-center">
         <Link href="/">
-            <Image src={theme === 'dark' ? lightLogo : darkLogo} alt="Logo" width={75} height={75} className="sm:w-125 sm:h-125" />
+            <Image src={theme === 'dark' ? lightLogo : darkLogo} alt="Logo" width={100} height={100} className="sm:w-125 sm:h-125" />
         </Link>
       </div>
       <div className="flex h-5 items-center space-x-2 sm:space-x-4 text-xs sm:text-sm">
